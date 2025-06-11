28d51ca56ef7bcc6ba8d55d172ef219d7fdf8a4f
DibsOnIt <JoeRBrender@gmail.com>
2025-06-11 14:53:20 -0700

    Update isometric.js

--- js/isometric.js
+++ js/isometric.js
@@ -1,16 +1,12 @@
-import { TILE_SIZE } from './utils.js';
-
-export function isoToScreen(x, y) {
+export function isoToScreen(x, y, tileSize) {
   return {
-    x: (x - y) * (TILE_SIZE / 2) + 512,
-    y: (x + y) * (TILE_SIZE / 4) + 100
+    x: (x - y) * tileSize / 2,
+    y: (x + y) * tileSize / 4
   };
 }
 
-export function screenToIso(screenX, screenY) {
-  screenX -= 512;
-  screenY -= 100;
-  const x = (screenX / (TILE_SIZE / 2) + screenY / (TILE_SIZE / 4)) / 2;
-  const y = (screenY / (TILE_SIZE / 4) - screenX / (TILE_SIZE / 2)) / 2;
-  return { x: Math.floor(x), y: Math.floor(y) };
+export function screenToIso(screenX, screenY, tileSize) {
+  const isoX = (screenX / (tileSize / 2) + screenY / (tileSize / 4)) / 2;
+  const isoY = (screenY / (tileSize / 4) - screenX / (tileSize / 2)) / 2;
+  return { x: Math.floor(isoX), y: Math.floor(isoY) };
 }
