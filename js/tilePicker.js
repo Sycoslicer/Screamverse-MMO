fa870487589757da33da1031aa55e40d0e0d9f1e
DibsOnIt <JoeRBrender@gmail.com>
2025-06-11 16:05:42 -0700

    Update tilePicker.js

--- js/tilePicker.js
+++ js/tilePicker.js
@@ -1,15 +1,31 @@
-export function setupTilePicker(tileset, onSelect) {
-  const tilePicker = document.getElementById('tilePicker');
-  for (let i = 0; i < tileset.columns * (tileset.image.height / tileset.tileHeight); i++) {
-    const button = document.createElement('div');
-    button.classList.add('tile-button');
-    button.style.backgroundImage = `url(tilesets/${tileset.image.src.split('/').pop()})`;
-    
-    const x = (i % tileset.columns) * tileset.tileWidth;
-    const y = Math.floor(i / tileset.columns) * tileset.tileHeight;
-    button.style.backgroundPosition = `-${x}px -${y}px`;
-    
-    button.addEventListener('click', () => onSelect(i));
-    tilePicker.appendChild(button);
-  }
+import { TILE_WIDTH, TILE_HEIGHT, TILESET_COLUMNS } from './isometric.js';
+
+export let selectedTile = 0;
+export let tilesetImage = new Image();
+
+export function loadTilePicker() {
+  const tilePicker = document.getElementById("tilePicker");
+  tilePicker.innerHTML = '';
+
+  tilesetImage.src = './tilesets/tileset.png';
+  tilesetImage.onload = () => {
+    const numTiles = TILESET_COLUMNS * (tilesetImage.height / TILE_HEIGHT);
+
+    for (let i = 0; i < numTiles; i++) {
+      const tileDiv = document.createElement("div");
+      tileDiv.className = "tile-button";
+      tileDiv.style.backgroundImage = `url(./tilesets/tileset.png)`;
+
+      const x = (i % TILESET_COLUMNS) * TILE_WIDTH;
+      const y = Math.floor(i / TILESET_COLUMNS) * TILE_HEIGHT;
+      tileDiv.style.backgroundPosition = `-${x}px -${y}px`;
+      tileDiv.dataset.tileIndex = i;
+
+      tileDiv.addEventListener('click', () => {
+        selectedTile = parseInt(tileDiv.dataset.tileIndex);
+      });
+
+      tilePicker.appendChild(tileDiv);
+    }
+  };
 }
