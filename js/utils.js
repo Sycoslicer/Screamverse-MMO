e0171d56f72b34c86e3acf621b85df0c0fe8d2d4
DibsOnIt <JoeRBrender@gmail.com>
2025-06-11 15:32:50 -0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,13 @@
-export function create2DArray(width, height, defaultValue = 0) {
-  return Array.from({ length: width }, () => Array(height).fill(defaultValue));
+export function worldToScreen(x, y, tileWidth, tileHeight, offsetX, offsetY, zoom) {
+  let screenX = (x - y) * tileWidth / 2 * zoom + offsetX;
+  let screenY = (x + y) * tileHeight / 4 * zoom + offsetY;
+  return { screenX, screenY };
 }
 
-export function clone(obj) {
-  return JSON.parse(JSON.stringify(obj));
+export function screenToWorld(screenX, screenY, tileWidth, tileHeight, offsetX, offsetY, zoom) {
+  screenX = (screenX - offsetX) / zoom;
+  screenY = (screenY - offsetY) / zoom;
+  let worldX = (screenY / (tileHeight / 4) + screenX / (tileWidth / 2)) / 2;
+  let worldY = (screenY / (tileHeight / 4) - screenX / (tileWidth / 2)) / 2;
+  return { worldX: Math.floor(worldX), worldY: Math.floor(worldY) };
 }
