e91d767ebb2f0b714ae72d86eba52ecabc93aa37
DibsOnIt <JoeRBrender@gmail.com>
2025-06-11 18:15:59 -0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,8 @@
-export function screenToIso(x, y, tileWidth, tileHeight) {
-  let isoX = (x - y) * tileWidth / 2;
-  let isoY = (x + y) * tileHeight / 2;
-  return { isoX, isoY };
-}
+export const TILE_SIZE = 32;
 
-export function isoToScreen(isoX, isoY, tileWidth, tileHeight) {
-  let x = (isoX / (tileWidth / 2) + isoY / (tileHeight / 2)) / 2;
-  let y = (isoY / (tileHeight / 2) - (isoX / (tileWidth / 2))) / 2;
-  return { x, y };
+export function getTileSource(tileIndex) {
+    return {
+        sx: (tileIndex - 1) * TILE_SIZE,
+        sy: 0
+    };
 }
