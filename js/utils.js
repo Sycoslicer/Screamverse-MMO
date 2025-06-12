47dc8c21186f876fc8fde81cd1497484f41472c9
DibsOnIt <JoeRBrender@gmail.com>
2025-06-12 00:43:15 -0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,15 @@
-export function isoToScreen(x, y, tileWidth, tileHeight) {
-    const screenX = (x - y) * (tileWidth / 2);
-    const screenY = (x + y) * (tileHeight / 2);
-    return { screenX, screenY };
+export function saveToFile(content, filename) {
+  const blob = new Blob([content], {type: 'application/json'});
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(url);
 }
 
-export function screenToIso(screenX, screenY, tileWidth, tileHeight) {
-    const x = (screenX / (tileWidth / 2) + screenY / (tileHeight / 2)) / 2;
-    const y = (screenY / (tileHeight / 2) - screenX / (tileWidth / 2)) / 2;
-    return { x, y };
-}
-
-export function clamp(value, min, max) {
-    return Math.max(min, Math.min(max, value));
+export function loadFromFile(file, callback) {
+  const reader = new FileReader();
+  reader.onload = () => callback(JSON.parse(reader.result));
+  reader.readAsText(file);
 }
