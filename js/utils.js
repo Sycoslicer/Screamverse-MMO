15718f703f58896aae9b717a8cc4024e5692e9a6
DibsOnIt <JoeRBrender@gmail.com>
2025-06-11 18:54:38 -0700

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,13 +1,15 @@
-export function loadImage(src) {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.onerror = reject;
-        img.src = src;
-    });
+export function isoToScreen(x, y, tileWidth, tileHeight) {
+    const screenX = (x - y) * (tileWidth / 2);
+    const screenY = (x + y) * (tileHeight / 2);
+    return { screenX, screenY };
 }
 
-export function getTilesetList() {
-    return fetch('./tilesets/tilesets.json')
-        .then(response => response.json());
+export function screenToIso(screenX, screenY, tileWidth, tileHeight) {
+    const x = (screenX / (tileWidth / 2) + screenY / (tileHeight / 2)) / 2;
+    const y = (screenY / (tileHeight / 2) - screenX / (tileWidth / 2)) / 2;
+    return { x, y };
+}
+
+export function clamp(value, min, max) {
+    return Math.max(min, Math.min(max, value));
 }
