b5267d48c601a56c7454778558a1881a068a13cd
DibsOnIt <JoeRBrender@gmail.com>
2025-06-11 18:32:26 -0700

    Update tilePicker.js

--- js/tilePicker.js
+++ js/tilePicker.js
@@ -1,37 +1,57 @@
-import { TILE_SIZE, loadTilesetImage, getTileSource } from './utils.js';
+import { loadImage, getTilesetList } from './utils.js';
 
-let TILESET_IMAGE = null;
-let currentTilesetPath = 'tilesets/tileset.png';
-let numTiles = 0;
-
-// Load the selected tileset image
-export function loadTileset(path, callback) {
-    currentTilesetPath = path;
-    const image = new Image();
-    image.onload = () => {
-        TILESET_IMAGE = image;
-        numTiles = Math.floor(image.width / TILE_SIZE);
-        callback();
-    };
-    image.src = path;
-}
-
-export function drawTilePicker(container) {
-    container.innerHTML = '';
-    for (let i = 0; i < numTiles; i++) {
-        const button = document.createElement('div');
-        button.className = 'tile-button';
-        button.dataset.tile = i + 1;
-        button.style.backgroundImage = `url(${currentTilesetPath})`;
-        button.style.backgroundPosition = `-${i * TILE_SIZE}px 0px`;
-        container.appendChild(button);
+export class TilePicker {
+    constructor(onTileSelect) {
+        this.onTileSelect = onTileSelect;
+        this.tiles = [];
+        this.selectedTile = null;
+        this.loadTilesets();
     }
-}
 
-export function getTilesetImage() {
-    return TILESET_IMAGE;
-}
+    async loadTilesets() {
+        const tilesetList = await getTilesetList();
+        for (let name of tilesetList) {
+            await this.loadTileset(`./tilesets/${name}`);
+        }
+        this.render();
+    }
 
-export function getNumTiles() {
-    return numTiles;
+    async loadTileset(src) {
+        const img = await loadImage(src);
+        const tileSize = 32;
+        const tilesPerRow = Math.floor(img.width / tileSize);
+        const tilesPerCol = Math.floor(img.height / tileSize);
+
+        for (let y = 0; y < tilesPerCol; y++) {
+            for (let x = 0; x < tilesPerRow; x++) {
+                this.tiles.push({
+                    image: img,
+                    x: x * tileSize,
+                    y: y * tileSize
+                });
+            }
+        }
+    }
+
+    render() {
+        const picker = document.getElementById("tilePicker");
+        picker.innerHTML = '';
+
+        for (let i = 0; i < this.tiles.length; i++) {
+            const tile = this.tiles[i];
+            const canvas = document.createElement("canvas");
+            canvas.width = 32;
+            canvas.height = 32;
+            const ctx = canvas.getContext("2d");
+            ctx.drawImage(tile.image, tile.x, tile.y, 32, 32, 0, 0, 32, 32);
+            canvas.classList.add("tile-button");
+
+            canvas.addEventListener("click", () => {
+                this.selectedTile = tile;
+                this.onTileSelect(tile);
+            });
+
+            picker.appendChild(canvas);
+        }
+    }
 }
